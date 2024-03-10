9003ce90dadda494886e572860c4f64f000eddae
uvsq22201695 <alexandre.francois@ens.uvsq.fr>
2024-03-10 14:45:44 +0100

    Correction x3 de l'algorithme de Dijkstra

--- dijkstra.py
+++ dijkstra.py
@@ -5,8 +5,6 @@ from queue import Queue, QueueItem
 def pch(graph, start: int) -> dict[int, tuple[float, list[Any]]]:
     """
     Retourne le plus court chemin entre debut et fin dans le graphe.
-    https://bradfieldcs.com/algos/graphs/dijkstras-algorithm/
-    https://python.plainenglish.io/implementing-priority-queue-in-python-with-heapq-168d084f179d
     :param graph: Un objet contenant le graphe.
     :param start: Le sommet de départ.
     :return: Le plus court chemin entre debut et fin.
@@ -23,6 +21,8 @@ def pch(graph, start: int) -> dict[int, tuple[float, list[Any]]]:
     # On met le coût du sommet de départ à 0 et le chemin à [start]
     distances[start] = (0, [start])
 
+    visited = set()  # On initialise un ensemble pour garder les sommets visités
+
     while queue:
 
         # On récupère et supprime le sommet avec le coût le plus bas de la file de priorité
@@ -30,12 +30,23 @@ def pch(graph, start: int) -> dict[int, tuple[float, list[Any]]]:
         cout = element.get_priority()
         sommet, chemin = element.get_item()
 
+        # Si le sommet a déjà été visité, on passe au suivant
+        if sommet in visited:
+            continue
+
+        # On ajoute le sommet actuel à la liste des sommets visités
+        visited.add(sommet)
+
         for neighbour in graph.vertices[sommet].get_neighbours():  # Pour chaque voisin du sommet actuel
-            nouveau_cout  = cout + neighbour.weight  # On calcule le coût du chemin actuel + le poids de l'arête
-            if nouveau_cout < distances[neighbour.vertex.identifier][0]:  # Si le nouveau coût est plus petit que le coût actuel
-                distances[neighbour.vertex.identifier] = (nouveau_cout, chemin + [neighbour.vertex.identifier])  # On met à jour le coût et le chemin
+            nouveau_cout = cout + neighbour.weight  # On calcule le coût du chemin actuel + le poids de l'arête
+
+            # Si le nouveau coût est plus petit que le coût actuel
+            if nouveau_cout < distances[neighbour.vertex.identifier][0]:
+                # On met à jour le coût et le chemin
+                distances[neighbour.vertex.identifier] = (nouveau_cout, chemin + [neighbour.vertex.identifier])
 
                 # On ajoute le voisin à la file de priorité avec le nouveau coût et le nouveau chemin
-                queue.add(QueueItem((neighbour.vertex.identifier, chemin + [neighbour.vertex.identifier]), nouveau_cout))
+                queue.add(QueueItem((neighbour.vertex.identifier, chemin + [neighbour.vertex.identifier]),
+                                    nouveau_cout))
 
     return distances
