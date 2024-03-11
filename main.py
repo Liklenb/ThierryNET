aff8006493a20fb5a0c815597730c54e6cf9bd2b
uvsq22201695 <alexandre.francois@ens.uvsq.fr>
2024-03-11 20:10:34 +0100

    Ajout de l'algorithme DFS pour vérifier la connexité du graphe
    - Implémenté l'algorithme de parcours en profondeur (DFS) pour évaluer la connexité dans les graphes.
    - Organisé les fonctions relatives aux graphes dans des dossiers structurés pour améliorer la lisibilité et la maintenance du code.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from dijkstra import pch
+from graph_algorithms.dfs import parcours_profondeur_rec
+from graph_algorithms.pathfinding import pch
 from graph import Graph
 
 
 if __name__ == '__main__':
     test_graph = Graph.load_file('graph.thierry')
-    d = pch(test_graph, 0)
-    print(d)
+    print(pch(test_graph, 0))  # Dijkstra
+    print(parcours_profondeur_rec(test_graph))  # Parcours en profondeur
